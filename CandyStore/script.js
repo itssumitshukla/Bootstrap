a893d5f6d5fbf39744b6d953ade0ecb0d299d020
itssumitshukla <itssumitshukla@gmail.com>
2021-03-03 20:10:54 -1000

    fix responsive issue and started to add scroll sticky using vanilla js on nav

---
+++ CandyStore/script.js
@@ -0,0 +1,12 @@
+
+let scrolled = document.querySelector('.scrolled');
+
+window.addEventListener('scroll', function(e){
+  // if(document.body.scrollTop > 50){
+  //   scrolled.className = 'nope';
+  //   console.log('YOOO')
+  // } else {
+  //   scrolled.className = 'scrolled';
+  //   console.log('nope')
+  // }
+});
