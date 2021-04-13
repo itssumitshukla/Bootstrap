4bbac8c75838db0c49c9c098def5960ca057de90
itssumitshukla <itssumitshukla@gmail.com>
2021-04-12 22:46:13 -1000

    testing modals

--- ChickenSandwichWar/script.js
+++ ChickenSandwichWar/script.js
@@ -9,3 +9,20 @@
     document.getElementById("screen").innerHTML = myWidth + "x" + myHeight;
   };
 })();
+
+let yyo = 'mcDonalds';
+
+//
+let modal = document.querySelectorAll('.card');
+let myArray = Array.from(modal)
+console.log(typeof(myArray))
+
+myArray.forEach(e=>{
+  console.log(e)
+  e.addEventListener('click',(tt)=>{
+    let inn = tt.target.alt;
+    if(inn == yyo){
+      console.log('BRUH THIS WORK')
+    }
+  })
+})
