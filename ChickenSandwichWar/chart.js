8987e14b388de90d3e130d0e4cc6f795c38b8094
itssumitshukla <itssumitshukla@gmail.com>
2021-06-09 23:18:23 -1000

    add buttons and change chart function

--- ChickenSandwichWar/chart.js
+++ ChickenSandwichWar/chart.js
@@ -72,12 +72,12 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['MouthFeel', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Chimkin Taste Chart',
+            data: [1, 2, 3, 4, 5],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                'red',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
@@ -85,14 +85,14 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
+                'blue',
+                'red',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 2
         }]
     }
 });
